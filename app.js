52dd1d63f0a1190bf44ce5c572f880ae39da91a4
crsvalle <cristian.valle@pursuit.org>
2023-10-30 19:54:30 -0400

    added server.js & app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+//DEPENDENCIES
+const cors = require('cors');
+const express = require('express');
+
+//CONFIGURATION
+const app = express();
+
+//MIDDLEWARE
+app.use(cors())
+app.use(express.json());
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send("Welcome!")
+})
+
+app.get("*", (req, res) =>{
+    res.status(404).send("Page not found!");
+})
+
+//EXPORT
+module.exports = app
