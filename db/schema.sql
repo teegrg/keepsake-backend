c14f4d71090f28ebfa44c1e8319b801e300c08d1
crsvalle <cristian.valle@pursuit.org>
2023-10-30 21:30:46 -0400

    added schema.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,18 @@ DROP DATABASE IF EXISTS keepsake_dev;
 CREATE DATABASE keepsake_dev;
 
 \c keepsake_dev;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    firstName text,
+    lastName text,
+    address text,
+    email varchar(255) UNIQUE NOT NULL,
+    phone text,
+    verified boolean,
+    password varchar(255) NOT NULL,
+    created_at date DEFAULT current_date,
+    role text
+);
