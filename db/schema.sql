5cbd5738eb33dbae9036c160a342798f1309899c
Jerry John <jerryjohn@jerrys-air.myfiosgateway.com>
2023-10-30 21:30:48 -0400

    Commit 4

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,34 @@
 DROP DATABASE IF EXISTS keepsake_dev;
 CREATE DATABASE keepsake_dev;
 
-<<<<<<< HEAD
-\c keepsake_dev; 
-=======
 \c keepsake_dev;
->>>>>>> 31a667630a668710bd39811f98983841a5fc4c90
+
+DROP TABLE IF EXISTS user_reviews;
+
+CREATE TABLE user_reviews (
+  id SERIAL PRIMARY KEY,
+  author_id INTEGER REFERENCES users (user_id),
+  reviewed_id INTEGER REFERENCES users (user_id),
+  title TEXT NOT NULL, 
+  body TEXT NOT NULL,
+  rating INTEGER NOT NULL,
+
+);
+
+CREATE TABLE listing_reviews (
+  id SERIAL PRIMARY KEY,
+  author_id INTEGER REFERENCES users (user_id),
+  listing_id INTEGER REFERENCES users (user_id),
+  title TEXT NOT NULL, 
+  body TEXT NOT NULL,
+  rating INTEGER NOT NULL,
+
+);
+
+CREATE TABLE availability (
+    id SERIAL PRIMARY KEY
+    listing_id INTEGER REFERENCES user_id (user_id),
+    days_not_available DATE [],
+    min_days INTEGER NOT NULL,
+    max_days INTEGER NOT NULL,
+);
