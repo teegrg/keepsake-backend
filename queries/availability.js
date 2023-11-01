41323f5a415cf1d27fae86aee6991701817393a8
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-01 09:48:16 -0400

    create new querry built

--- queries/availability.js
+++ queries/availability.js
@@ -8,15 +8,29 @@ const getAllAvailability = async () => {
     } catch (error) {
       return error;
     }
-  };
+};
 
 
 // ONE AVAILABILITY
 const getAvailability = async (id) => {
-try {
-    const oneAvailability = await db.one("SELECT * FROM availability WHERE id=$1", id);
-    return oneAvailability;
-} catch (error) {
-    return error;
-}
+    try {
+        const oneAvailability = await db.one("SELECT * FROM availability WHERE id=$1", id);
+        return oneAvailability;
+    } catch (error) {
+        return error;
+    }
+};
+
+
+//CREATE NEW AVAILABILITY
+const createAvailability = async (availability) => {
+    try {
+        const newAvailability = await db.one(
+        "INSERT INTO availability (listing_id, days_not_available, min_days, max_days) VALUES($1, $2, $3, $4) RETURNING *",
+        [availability.listing_id, availability.days_not_available, availability.min_days, availability.max_days]
+        );
+        return newAvailability;
+    } catch (error) {
+        return error;
+    }
 };
