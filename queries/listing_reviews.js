1b2f488174390e9248cb50e00348bc95504f9861
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-01 09:23:23 -0400

    one listing review function created

--- queries/listing_reviews.js
+++ queries/listing_reviews.js
@@ -1,10 +1,21 @@
 const db = require("../db/dbConfig.js");
 
-//ALL Bookmarks
-const getAllReviews = async () => {
+//ALL LISTING REVIEWS
+const getAllListingReviews = async () => {
     try {
       const allReviews = await db.any("SELECT * FROM listing_reviews");
       return allReviews;
+    } catch (error) {
+      return error;
+    }
+  };
+
+
+  // ONE LISTING_REVIEWS
+  const getListingReview = async (id) => {
+    try {
+      const oneReview = await db.one("SELECT * FROM listing_reviews WHERE id=$1", id);
+      return oneReview;
     } catch (error) {
       return error;
     }
