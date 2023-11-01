9cd25fa94c6a6c124189d23cc37cdbb2722d1887
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-10-31 21:39:52 -0400

    get single user function getuser created

--- queries/users.js
+++ queries/users.js
@@ -9,3 +9,14 @@ const getAllUsers = async () => {
       return error;
     }
   };
+
+
+  // ONE Bookmark
+  const getUsers = async (id) => {
+    try {
+      const oneUser = await db.one("SELECT * FROM users WHERE id=$1", id);
+      return oneUser;
+    } catch (error) {
+      return error;
+    }
+  };
