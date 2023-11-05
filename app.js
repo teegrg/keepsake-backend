91045f057b31abf6efe486611bd632e873e6fa95
crsvalle <cristian.valle@pursuit.org>
2023-11-05 11:27:22 -0500

    added availability controller

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ app.get('/', (req, res) => {
 const user_reviewController = require("./controllers/user_reviewController.js");
 app.use("/userReviews", user_reviewController);
 
-// const listing_reviewController = require("./controllers/listing_reviewController.js");
-// app.use("/listingReviews", listing_reviewController);
-
 
 // LISTINGS ROUTES
 const listings = require("./controllers/listingsController");
---
+++ controllers/availabilityController.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const availability = express.Router({mergeParams: true});
+
+const {getAvailability} = require('../queries/availability')
+
+
+// // SHOW
+availability.get("/", async (req, res) => {
+  const { listingId } = req.params;
+  console.log('test', listingId)
+  const availability = await getAvailability(listingId);
+  if (availability) {
+    res.json(availability);
+  }
+  else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+module.exports = availability;
