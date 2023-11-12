d28d7a4a9b486de2ef8f8b86560fd322a45e677a
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-12 14:56:50 -0500

    querry for getting one property type

--- queries/property_type.js
+++ queries/property_type.js
@@ -9,3 +9,15 @@ const getAllPropertyType = async () => {
       return error;
     }
   };
+
+
+
+// ONE PROPERTY_TYPE
+const getOnePropertyType = async (id) => {
+try {
+    const OnePropertyType = await db.one("SELECT * FROM property_type_lookup WHERE id=$1", id);
+    return OnePropertyType;
+} catch (error) {
+    return error;
+}
+};
